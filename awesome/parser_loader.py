55bcd956658acba92f1bb77cb149cc6f949d0bac
mingrammer <k239507@gmail.com>
2017-09-11 12:05:59 +0900

    Supports awesome-android/ios

--- awesome/parser_loader.py
+++ awesome/parser_loader.py
@@ -1,6 +1,8 @@
+from .parsers.android import AwesomeAndroidParser
 from .parsers.awesome import AwesomeParser
 from .parsers.elixir import AwesomeElixirParser
 from .parsers.go import AwesomeGoParser
+from .parsers.ios import AwesomeIosParser
 from .parsers.java import AwesomeJavaParser
 from .parsers.javascript import AwesomeJavaScriptParser
 from .parsers.php import AwesomePHPParser
@@ -13,9 +15,11 @@ from .parsers.swift import AwesomeSwiftParser
 
 def load_parsers():
     return {
+        'android': AwesomeAndroidParser,
         'awesome': AwesomeParser,
         'elixir': AwesomeElixirParser,
         'go': AwesomeGoParser,
+        'ios': AwesomeIosParser,
         'java': AwesomeJavaParser,
         'javascript': AwesomeJavaScriptParser,
         'php': AwesomePHPParser,
---
+++ awesome/parsers/android.py
@@ -0,0 +1,37 @@
+from . import AbstractAwesomeParser
+
+
+class AwesomeAndroidParser(AbstractAwesomeParser):
+    AWESOME_TITLE = 'android'
+    AWESOME_README_URL = 'https://raw.githubusercontent.com/JStumpp/awesome-android/master/readme.md'
+
+    def find_content(self):
+        readme = self.read_readme()
+        content = readme.split('- [Contributing](#contributing)')[1]
+        lines = []
+        for line in content.split('\n'):
+            lines.append(line)
+        return lines
+
+    def parse_awesome_content(self, content):
+        awesome_blocks = []
+        for line in content:
+            # Parse the header title
+            if line.startswith('##'):
+                plain_title = self.parse_category_title(line)
+                awesome_blocks.append({
+                    'type': 'category',
+                    'line': plain_title,
+                })
+            # Parse the list item
+            elif line.strip().startswith('* [') or line.strip().startswith('- ['):
+                plain_line, link = self.parse_link_line(line)
+                awesome_blocks.append({
+                    'type': 'awesome',
+                    'line': plain_line,
+                    'link': link,
+                })
+            # Ignore last useless parts
+            elif line.startswith('## Contributing'):
+                break
+        return awesome_blocks
